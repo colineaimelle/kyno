b4acf02a515380c68c2422221817758a7088f6f7
colineaimelle <colinemngl@gmail.com>
2026-06-15 22:00:16 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -25,9 +25,7 @@ async function saveToSupabase(data, statut) {
   });
 }
 
-export default async function handler(req, res) {
-  // ... reste du code
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
